9c445220302abe3150035d01cfbdc863e9f93b34
maki-max <137396797+maki-max@users.noreply.github.com>
2024-05-19 19:49:14 +0300

    Add files via upload

---
+++ new java/index.js
@@ -0,0 +1,92 @@
+
+// let zero = document.getElementById("zero");
+// let x = document.getElementById("in");
+// let count = 0;
+// x.onclick=function(){
+//      count ++;
+// zero.textContent = count;
+
+// }
+
+
+// let zero1 = document.getElementById("zero");
+// let x1 = document.getElementById("de");
+// let count1 = 0;
+// x1.onclick=function(){
+//      count1 --;
+// zero1.textContent = count1;
+
+// }
+
+
+
+
+// let x = rundom;
+// rundom = Math.random(Math.floor(0+1))*6;
+// Math.floor(Math.random()*6)+1;
+// console.log(rundom);
+
+
+// let x = Math.random();
+// document.getElementById("my").onclick=function(){
+
+//      document.getElementById("my").textContent = x
+// }
+
+
+// let w = document.getElementById("myage").value;
+// let y = document.getElementById("sub");
+
+// y.onclick=function(){
+// if(w < 20){
+//      alert("hello");
+// }
+// else(w == 20){
+//      alert("nn");
+// }
+
+
+// }
+
+// let x = document.getElementById("visa");
+// let y =  document.getElementById("pay");
+// let z =  document.getElementById("sub");
+
+// z.onlcick = function(){
+//      if (x.checked){
+//           alert("hello");
+//      }
+//      else{
+//           alert("non");
+//      }
+// }
+
+// let day = 7;
+// switch(day){
+// case 1: day==1;
+// console.log("it's monday");
+// break;
+// case 2: day==2;
+// console.log("it's tuesday");
+// break;
+// case 3: day==7;
+// console.log("it's wed");
+// break;
+// default:
+//      console.log("none")
+// }
+
+// let m = "maki";
+// m.slice(3);
+// console.log(m);
+
+
+//  for(let i=0; i>=3; i++){
+//      alert("hello");
+// } 
+
+function happybirthday(){
+     console.log("hello");
+     console.log("hello madiye")
+}
+happybirthday();
---
+++ new java/index2.js
@@ -0,0 +1,126 @@
+// let x = document.getElementById("sand");
+// x.onclick = function(){
+//     alert("hello");
+// }
+//salad
+// function change2(){
+// let y =  document.getElementById("juc");
+// let f =  document.getElementById("sal");
+// let m = document.getElementById("salad");
+// let z = document.getElementById("juc1");
+// let b =  document.getElementById("cc");
+// let c =  document.getElementById("juc3");
+
+// m.onclick = function(){
+//    y.innerHTML = "mango";
+// z.innerHTML = "yellow";
+// b.innerHTML = "pink";
+// c.innerHTML = "car";
+//     var img = document.getElementById("sal");
+//    img.src = "cafe1.jpg";
+//    var img = document.getElementById("sal1");
+//    img.src = "cafe1.jpg";
+//    var img = document.getElementById("sal3");
+//    img.src = "cafe1.jpg";
+//    var img = document.getElementById("sal2");
+//    img.src = "cafe1.jpg";
+// }
+// }
+
+// change2();
+// //desert
+
+// function change3(){
+//     let y =  document.getElementById("juc");
+//     let f =  document.getElementById("sal");
+//     let m = document.getElementById("drink");
+//     let z = document.getElementById("juc1");
+//     let b =  document.getElementById("cc");
+//     let c =  document.getElementById("juc3");
+
+//     m.onclick = function(){
+//        y.innerHTML = "juce";
+//     z.innerHTML = "yell";
+//     b.innerHTML = "pk";
+//     c.innerHTML = "cupr";
+//         var img = document.getElementById("sal");
+//        img.src = "image/iced.jpg";
+//        var img = document.getElementById("sal1");
+//        img.src = "image/ice.png";
+//        var img = document.getElementById("sal3");
+//        img.src = "image/macc.jpg";
+//        var img = document.getElementById("sal2");
+//        img.src = "image/ice.jpg";
+//     }
+//     }
+
+//     change3();
+//     //drink
+//     function change4(){
+//         let y =  document.getElementById("juc");
+//         let f =  document.getElementById("sal");
+//         let m = document.getElementById("cake");
+//         let z = document.getElementById("juc1");
+//         let b =  document.getElementById("cc");
+//         let c =  document.getElementById("juc3");
+
+//         m.onclick = function(){
+//            y.innerHTML = "Black <br> Forest";
+//         z.innerHTML = "Chocolate <br> Tonka";
+//         b.innerHTML = "Cream <br> Puff";
+//         c.innerHTML = "Cheese <br> Cake";
+//             var img = document.getElementById("sal");
+//            img.src = "image/bla.jpg";
+//            var img = document.getElementById("sal1");
+//            img.src = "image/choco.jpg";
+//            var img = document.getElementById("sal3");
+//            img.src = "image/cheese.jpg";
+//            var img = document.getElementById("sal2");
+//            img.src = "image/custors.jpg";
+//         }
+//         }
+
+//         change4();
+
+//         function change5(){
+//          let y =  document.getElementById("juc");
+//          let f =  document.getElementById("sal");
+//          let m = document.getElementById("sand");
+//          let z = document.getElementById("juc1");
+//          let b =  document.getElementById("cc");
+//          let c =  document.getElementById("juc3");
+
+//          m.onclick = function(){
+//             y.innerHTML = "ma";
+//          z.innerHTML = "yel";
+//          b.innerHTML = "pink";
+//          c.innerHTML = "ca";
+//              var img = document.getElementById("sal");
+//             img.src = "cafe1.jpg";
+//             var img = document.getElementById("sal1");
+//             img.src = "cafe1.jpg";
+//             var img = document.getElementById("sal3");
+//             img.src = "cafe1.jpg";
+//             var img = document.getElementById("sal2");
+//             img.src = "cafe1.jpg";
+//          }
+//          }
+
+//          change5();
+
+function showSidebar() {
+   const sidebar = document.querySelector('.sidebar')
+   sidebar.style.display = 'flex'
+ }
+ function hideSidebar() {
+   const sidebar = document.querySelector('.sidebar')
+   sidebar.style.display = 'none'
+ }
+
+
+
+         $("#menu").click(function(){
+            $("nav").toggle();
+
+
+            });
